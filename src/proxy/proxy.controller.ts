f562e4fe1090270bfb29fb2c7c0b15e91aa8d417
conyeu <tuanit.nguyenquoc@gmail.com>
2021-08-11 20:42:10 +0700

    0

--- src/proxy/proxy.controller.ts
+++ src/proxy/proxy.controller.ts
@@ -34,8 +34,8 @@ export class ProxyController {
   constructor(private http: HttpService) {}
 
 
-  @Get(':url(*)')
-  async sendProxy(@Param('url') url: string, @Req() req: Request, @Res() res: Response) {
+  @Get()
+  async sendProxy(@Query('url') url: string, @Req() req: Request, @Res() res: Response) {
 
     let headers = {
       'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
