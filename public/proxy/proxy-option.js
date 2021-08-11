860646703033d3d7073f4332f9c43aaa237ce4e7
conyeu <tuanit.nguyenquoc@gmail.com>
2021-08-11 22:09:27 +0700

    0

--- public/proxy/proxy-option.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ProxyOption = void 0;
-class ProxyOption {
-}
-exports.ProxyOption = ProxyOption;
-//# sourceMappingURL=proxy-option.js.map
--- public/proxy/proxy.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { HttpService } from '@nestjs/axios';
-import { Request } from 'express';
-export declare class ProxyController {
-    private http;
-    constructor(http: HttpService);
-    sendProxy(url: string, req: Request): Promise<any>;
-}
